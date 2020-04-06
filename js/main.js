072b5fa5664756e8ac69d6dc51beda8b8482830a
Martin Schilliger <martin.schilliger@rettung-sg.ch>
2020-04-07 00:45:22 +0200

    Umschalten zwischen Streams funktioniert fetch in Promise

--- js/main.js
+++ js/main.js
@@ -1,45 +1,103 @@
 import JSMpeg from "@cycjimmy/jsmpeg-player";
 
 var streams = [1, 2, 3];
-var streamsData = [];
-var kamerasauPlayers = [];
+var kamerasauPlayer = {};
+var activeStreams;
 
-function readStreamsData() {
-  for (let stream of streams) {
-    fetch("https://kamerasau.martin-apps.ch/api-" + stream)
-      .then(function(response) {
-        return response.text();
-      })
-      .then(function(body) {
-        streamsData[stream] = JSON.parse(body);
-      });
+function playStream(stream) {
+  if (
+    typeof stream == "undefined" &&
+    !kamerasauPlayer.stream &&
+    activeStreams.length
+  ) {
+    stream = activeStreams[0];
+  } else if (typeof stream == "undefined") {
+    return false;
   }
-}
-setInterval(function() {
-  readStreamsData();
-}, 1000);
 
-for (let stream of streams) {
-  setTimeout(function() {
-    kamerasauPlayers[stream] = new JSMpeg.VideoElement(
-      "#videoWrapper" + stream,
+  if (kamerasauPlayer.stream == stream) {
+    return true;
+  } else {
+    kamerasauPlayer.stream = stream;
+  }
+
+  if (kamerasauPlayer.player) {
+    kamerasauPlayer.player.destroy();
+  }
+
+  (function() {
+    kamerasauPlayer.player = new JSMpeg.VideoElement(
+      "#videoWrapper",
       "wss://kamerasau.martin-apps.ch/websocket-" + stream + "/",
       {
         control: false,
         hooks: {
           load: function() {
             document.getElementById("textWrapper").style.display = "none";
-            console.log("streamSuccessfullLoaded", stream);
+          },
+          destroy: function() {
+            document.getElementById("textWrapper").style.display = "";
           }
         }
+      },
+      {
+        disableWebAssembly: true //schneller Wechsel zwischen Streams machte sonst noch mehr Probleme
       }
     );
-  }, stream * 2500 - 2500);
+  })();
 }
+
+function watchStreamsData() {
+  Promise.all(
+    streams.map(x => {
+      return fetch("https://kamerasau.martin-apps.ch/api-" + x).then(function(
+        response
+      ) {
+        return response.json();
+      });
+    })
+  ).then(streamsData => {
+    var buttonDiv = document.getElementById("streamSwitcher");
+    activeStreams = [];
+
+    for (let data of streamsData) {
+      (function() {
+        var button = document.getElementById("cameraButton" + data.number);
+        if (data.stream_active) {
+          button.style.display = "inline-block";
+          activeStreams.push(data.number);
+        } else {
+          button.style.display = "";
+        }
+
+        if (activeStreams.length > 1) {
+          buttonDiv.style.display = "block";
+        } else {
+          buttonDiv.style.display = "";
+        }
+
+        if (activeStreams.length) {
+          document.getElementById("errorText").style.opacity = 0;
+        } else {
+          document.getElementById("errorText").style.opacity = 1;
+        }
+      })();
+    }
+    playStream();
+  });
+}
+setInterval(function() {
+  watchStreamsData();
+}, 1000);
+
+var buttons = document.querySelectorAll("#streamSwitcher .camera-switch");
+Array.prototype.forEach.call(buttons, function(button) {
+  button.addEventListener("click", function(e) {
+    let stream = Number(e.target.id.replace(/\D/g, ""));
+    playStream(stream);
+  });
+});
 
 setTimeout(function() {
   document.getElementById("errorText").style.opacity = 1;
 }, 2000);
-
-window.kamerasauPlayers = kamerasauPlayers;
-window.streamsData = streamsData;
