627ae3f549df8b069e3971e623102e62c6145dca
Martin Schilliger <martin.schilliger@rettung-sg.ch>
2020-04-06 10:41:48 +0200

    Alle drei Streams zeitversetzt, so funktioniert es provisorisch

--- js/main.js
+++ js/main.js
@@ -1,27 +1,51 @@
 import JSMpeg from "@cycjimmy/jsmpeg-player";
 
-window.customPlayer = new JSMpeg.VideoElement(
-  "#videoWrapper",
-  "wss://kamerasau.martin-apps.ch/websocket-2/",
-  {
-    autoSetWrapperSize: false,
-    control: false,
-    autoplay: true,
-    picMode: true
-  },
-  {
-    onEnded: function() {
-      console.log("onEnded");
-    },
-    onStalled: function() {
-      console.log("onStalled");
-    },
-    onSourceEstablished: function() {
-      document.getElementById("videoWrapper").style.opacity = 1;
-      document.getElementById("errorText").style.display = "none";
-    },
-    onSourceCompleted: function() {
-      console.log("onSourceCompleted");
-    }
-  }
-);
+let streams = [1, 2, 3];
+var kamerasauPlayers = [];
+var foundStreamSource = false;
+//
+// function streamSuccessfullLoaded(number) {
+//   console.log("streamSuccessfullLoaded", number);
+//   // var videoWrappers = document.getElementsByClassName("video-wrapper");
+//   // Array.prototype.forEach.call(videoWrappers, function(el, i) {
+//   //   el.style.opacity = 1;
+//   // });
+//
+//   for (let stream of streams) {
+//     if (stream == number) {
+//       console.log("stream == number", number);
+//       continue;
+//     }
+//     // if (!foundStreamSource) {
+//     //   kamerasauPlayers[stream].destroy();
+//     // }
+//   }
+//   foundStreamSource = true;
+// }
+
+for (let stream of streams) {
+  setTimeout(function() {
+    (function() {
+      // Keine Ahnung wieso, aber nur so funktioniert der Stream
+      kamerasauPlayers[stream] = new JSMpeg.VideoElement(
+        "#videoWrapper" + stream,
+        "wss://kamerasau.martin-apps.ch/websocket-" + stream + "/",
+        {
+          control: false,
+          hooks: {
+            load: function() {
+              document.getElementById("textWrapper").style.display = "none";
+              console.log("streamSuccessfullLoaded", stream);
+            }
+          }
+        }
+      );
+    })();
+  }, stream * 2500 - 2500);
+}
+
+setTimeout(function() {
+  document.getElementById("errorText").style.opacity = 1;
+}, 2000);
+
+window.kamerasauPlayers = kamerasauPlayers;
