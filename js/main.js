e3fa0fd4cd197a5b173525e33ca0a9d84ce69d38
Martin Schilliger <martin.schilliger@rettung-sg.ch>
2020-04-06 15:31:11 +0200

    api eingebaut, wird aber noch nicht verwendet

--- js/main.js
+++ js/main.js
@@ -1,46 +1,39 @@
 import JSMpeg from "@cycjimmy/jsmpeg-player";
 
-let streams = [1, 2, 3];
+var streams = [1, 2, 3];
+var streamsData = [];
 var kamerasauPlayers = [];
-var foundStreamSource = false;
-//
-// function streamSuccessfullLoaded(number) {
-//   console.log("streamSuccessfullLoaded", number);
-//   // var videoWrappers = document.getElementsByClassName("video-wrapper");
-//   // Array.prototype.forEach.call(videoWrappers, function(el, i) {
-//   //   el.style.opacity = 1;
-//   // });
-//
-//   for (let stream of streams) {
-//     if (stream == number) {
-//       console.log("stream == number", number);
-//       continue;
-//     }
-//     // if (!foundStreamSource) {
-//     //   kamerasauPlayers[stream].destroy();
-//     // }
-//   }
-//   foundStreamSource = true;
-// }
+
+function readStreamsData() {
+  for (let stream of streams) {
+    fetch("https://kamerasau.martin-apps.ch/api-" + stream)
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(body) {
+        streamsData[stream] = JSON.parse(body);
+      });
+  }
+}
+setInterval(function() {
+  readStreamsData();
+}, 1000);
 
 for (let stream of streams) {
   setTimeout(function() {
-    (function() {
-      // Keine Ahnung wieso, aber nur so funktioniert der Stream
-      kamerasauPlayers[stream] = new JSMpeg.VideoElement(
-        "#videoWrapper" + stream,
-        "wss://kamerasau.martin-apps.ch/websocket-" + stream + "/",
-        {
-          control: false,
-          hooks: {
-            load: function() {
-              document.getElementById("textWrapper").style.display = "none";
-              console.log("streamSuccessfullLoaded", stream);
-            }
+    kamerasauPlayers[stream] = new JSMpeg.VideoElement(
+      "#videoWrapper" + stream,
+      "wss://kamerasau.martin-apps.ch/websocket-" + stream + "/",
+      {
+        control: false,
+        hooks: {
+          load: function() {
+            document.getElementById("textWrapper").style.display = "none";
+            console.log("streamSuccessfullLoaded", stream);
           }
         }
-      );
-    })();
+      }
+    );
   }, stream * 2500 - 2500);
 }
 
@@ -49,3 +42,4 @@ setTimeout(function() {
 }, 2000);
 
 window.kamerasauPlayers = kamerasauPlayers;
+window.streamsData = streamsData;
